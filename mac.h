59df0d9cc200bc74f617be5b06cdb0c3e5f53c93
justin1196 <81173872+justin1196@users.noreply.github.com>
2021-12-25 05:12:35 +0900

    Update mac.h

---
+++ mac.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+struct Mac final {
+    static constexpr int SIZE = 6;
+
+    Mac() {}
+    Mac(const Mac& r) { memcpy(this->mac_, r.mac_, SIZE); }
+    Mac(const uint8_t* r) { memcpy(this->mac_, r, SIZE); }
+    Mac(const std::string& r);
+
+    Mac& operator = (const Mac& r) { memcpy(this->mac_, r.mac_, SIZE); return *this; }
+
+    explicit operator uint8_t*() const { return const_cast<uint8_t*>(mac_); }
+    explicit operator std::string() const;
+
+    bool operator == (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) == 0; }
+    bool operator != (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) != 0; }
+    bool operator < (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) < 0; }
+    bool operator > (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) > 0; }
+    bool operator <= (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) <= 0; }
+    bool operator >= (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) >= 0; }
+    bool operator == (const uint8_t* r) const { return memcmp(mac_, r, SIZE) == 0; }
+
+    void clear() {
+        *this = nullMac();
+    }
+
+    bool isNull() const {
+        return *this == nullMac();
+    }
+
+    bool isBroadcast() const { 
+        return *this == broadcastMac();
+    }
+
+    bool isMulticast() const { 
+        return mac_[0] == 0x01 && mac_[1] == 0x00 && mac_[2] == 0x5E && (mac_[3] & 0x80) == 0x00;
+    }
+
+    static Mac randomMac();
+    static Mac& nullMac();
+    static Mac& broadcastMac();
+
+protected:
+    uint8_t mac_[SIZE];
+};
+
+namespace std {
+    template<>
+    struct hash<Mac> {
+        size_t operator() (const Mac& r) const {
+            return std::_Hash_impl::hash(&r, Mac::SIZE);
+        }
+    };
+}
